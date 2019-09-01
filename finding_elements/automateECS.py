6ad1645eee8112517a68b66df5eb127dcd3b2655
waquar <waquar.alam92@gmail.com>
2019-09-02 00:57:00 +0530

    automate ecs done

--- finding_elements/automateECS.py
+++ finding_elements/automateECS.py
@@ -1,20 +1,29 @@
+
+
 from selenium import webdriver
-from selenium.webdriver.common.by import  By
 import time
-import os
-path = 'D:\\Selenium_Addon_Files\\IEDriverServer.exe'
-a = []
-os.environ['webdriver.ie.driver'] = path
-driver = webdriver.Ie(path)
-driver.get('someurl')
-time.sleep(4)
-handlealert = driver.switch_to.alert
-time.sleep(2)
-handlealert.send_keys('zzzz')
-handlealert.accept()
+from selenium.webdriver.chrome.options import Options
 
-b= driver.find_elements(By.XPATH, "//div[@class='hdrcell'][3]")
-b.__getitem__(a)
-print(a)
+chrome_options = Options()
+chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+#Change chrome driver path accordingly
+chrome_driver = "D:\\Selenium_Addon_Files\\chromedriver.exe"
+driver = webdriver.Chrome(chrome_driver, chrome_options=chrome_options)
+#driver.get("https://sp.hexaware.com/sites/tne/sitepages/expensehome.aspx")
+print(driver.title)
 
+a = ['7850' , '5000']
 
+for i in range(2, 32):
+    xpath = "//table[@class='obj row20px']//tr[" + str(i) + "]//td[3]"
+   # time.sleep(1)
+    amountElem = driver.find_element_by_xpath(xpath)
+    print(amountElem.text)
+    a.append(amountElem.text)
+
+total = list(map(int, a))
+a = (sum(total))
+b = a/12
+print(f"total money taken in ECS is {a}")
+print(f"monthly is {b}")
+print(f"total taken in shiftallowance is {250*308}")
--- finding_elements/multiple_elements.py
+++ finding_elements/multiple_elements.py
@@ -2,13 +2,15 @@ from selenium import  webdriver
 import os
 from selenium.webdriver.common.by import By
 import time
+
+
 path = 'D:\\Selenium_Addon_Files\\chromedriver.exe'
 a =[]
-
 os.environ['webdriver.chrome.driver'] = path
 driver = webdriver.Chrome(path)
 driver.get('https://learn.letskodeit.com/p/practice')
 driver.maximize_window()
+driver.refresh()
 target_link = driver.find_elements(By.TAG_NAME, 'td')
 for item in target_link:
     a.append(item.text)
