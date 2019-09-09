4d5c1eef3a5d254b699b1e302765c99c565dcb22
waquar <waquar.alam92@gmail.com>
2019-09-09 16:09:05 +0530

    added screenshots

---
+++ Actions_Iframes_javascripthandler/autocomplete.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+import os
+path= "D:\\Selenium_Addon_Files\\chromedriver.exe"
+
+os.environ['webdriver.chrome.driver'] = path
+driver = webdriver.Chrome(path)
+driver.set_window_position(1400,0)
+driver.maximize_window()
+#driver.implicitly_wait(5)
+driver.get("https://www.southwest.com/")
+
+city = driver.find_element_by_id("LandingAirBookingSearchForm_originationAirportCode")
+if city is not None:
+    print(city)
+time.sleep(10)
+city.clear()
+city.send_keys("NEW")
+try:
+    xpath = driver.find_element_by_xpath("//ul[@id='LandingAirBookingSearchForm_originationAirportCode--menu']/li[4]/button[@type='button']")
+    xpath.click()
+except ValueError:
+    print("try unexecuted")
+time.sleep(3)
+driver.close()
---
+++ Actions_Iframes_javascripthandler/screenshot.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+import os
+path= "D:\\Selenium_Addon_Files\\chromedriver.exe"
+
+os.environ['webdriver.chrome.driver'] = path
+driver = webdriver.Chrome(path)
+driver.set_window_position(1400,0)
+driver.maximize_window()
+driver.implicitly_wait(4)
+
+driver.get("https://learn.letskodeit.com/")
+driver.find_element_by_link_text('Login').click()
+username= driver.find_element_by_id("user_email")
+password = driver.find_element_by_id("user_password")
+login = driver.find_element_by_name('commit')
+username.send_keys('waq@gmail.com')
+password.send_keys('123ear')
+login.click()
+filelocation = "D:\\Export_path\\1.png"
+
+try:
+    driver.save_screenshot(filelocation)
+    print(f"file saved at this location:-- {filelocation}")
+except NotADirectoryError:
+    print("not location issue")
+
