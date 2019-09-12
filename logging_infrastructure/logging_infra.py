e706680793763c012bce3b71ccafc9d3936ebf80
waquar <waquar.alam92@gmail.com>
2019-09-12 16:53:14 +0530

    added logger

---
+++ logging_infrastructure/logging_infra.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.basicConfig(filename="test.log", level = logging.DEBUG)
+logging.warning("check")
+logging.info("check info")
+logging.debug("check debug")
+logging.error("check error")
