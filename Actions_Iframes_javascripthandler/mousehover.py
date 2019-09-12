8e178760f07acfcec4c6f4d5999800cf19a692de
waquar <waquar.alam92@gmail.com>
2019-09-12 13:06:44 +0530

    mousehover

---
+++ Actions_Iframes_javascripthandler/mousehover.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+import os, time
+path= "D:\\Selenium_Addon_Files\\chromedriver.exe"
+from selenium.webdriver.common.by import By
+from  selenium.webdriver import  ActionChains                      #used for hovering
+
+class hover():
+    def test(self):
+        os.environ['wendriver.chrome.driver'] = path
+        driver = webdriver.Chrome(path)
+        driver.set_window_position(1400, 0)
+        driver.maximize_window()
+        driver.implicitly_wait(3)
+        driver.get("https://learn.letskodeit.com/p/practice")
+        hoverbutton = driver.find_element_by_id('mousehover')
+
+        try:
+            action = ActionChains(driver)
+            action.move_to_element(hoverbutton).perform()
+            print("hovered mouse")
+            time.sleep(3)
+            link1 = driver.find_element(By.XPATH, ".//div[@class = 'mousehover']//a[text()='Reload']")
+            if link1.is_displayed():
+                print("xpath is correct")
+            else:
+                print("xpath not found")
+            action.move_to_element(link1).click().perform()
+            print("clicked item")
+        except:
+            print("some error")
+
+h = hover()
+h.test()
